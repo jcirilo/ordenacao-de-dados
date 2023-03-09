aed789f06fb1931d95effb54118fabd31b7d37bf
João Cirilo <jvscirilo12@gmail.com>
2023-03-09 12:42:17 -0300

    refatoração: função 'select' intercalada

--- include/selection_sort.h
+++ include/selection_sort.h
@@ -2,5 +2,6 @@
 #define SELECTION_SORT_H_INCLUDED
 
 void selection_sort( int *arr, int arr_size);
+int select (int *arr, int arr_size, int first_index);
 
 #endif
--- src/selection_sort.c
+++ src/selection_sort.c
@@ -1,12 +1,24 @@
-#include "select.h"
 #include "selection_sort.h"
 #include "swap.h"
+
+int select (int *arr, int arr_size, int first_index)
+{    
+    int lower = first_index;
+
+    for (int i = first_index + 1; i < arr_size; i++)
+    {
+        if (arr[i] < arr[lower])
+        lower = i;
+    }
+
+    return lower;
+}
 
 void selection_sort( int *arr, int arr_size)
 {
     for (int i = 0; i < arr_size ; i++)
     {
-        int lower_index = select (arr, arr_size, i);
+        int lower_index = select(arr, arr_size, i);
 
         if (lower_index != i)
         swap(arr, lower_index, i);
